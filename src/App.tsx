bf3101eb86132a626f02ebed6e2cec96b686e8de
Martin Petrov <martinmpetrov@hotmail.com>
2023-10-05 07:14:40 +0300

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Center,
+  Card,
+  Image,
+  Stack,
+  Flex,
+  Text,
+  Avatar,
+  Button,
+  fr,
+  usePrismaneTheme,
+} from "@prismane/core";
 
 function App() {
+  const { toggleThemeMode } = usePrismaneTheme();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Center
+      w="100vw"
+      h="100vh"
+      direction="column"
+      gap={fr(6)}
+      bg={["base", 300]}
+    >
+      <Card w={720} h={400} direction="row" gap={fr(6)}>
+        <Image
+          src="https://www.teahub.io/photos/full/66-664490_coffee-cup-minimal-hd-wallpaper-aesthetic-coffee-cup.jpg"
+          w={360}
+          h="100%"
+          br="md"
+          fit="cover"
+        />
+        <Stack>
+          <Text
+            as="h2"
+            cl={(theme) =>
+              theme.mode === "dark" ? ["base", 100] : ["base", 900]
+            }
+          >
+            How to make the perfect morning coffee, according to science
+          </Text>
+          <Text
+            cl={(theme) =>
+              theme.mode === "dark" ? ["base", 300] : ["base", 700]
+            }
+          >
+            Brewing the perfect cup of coffee is both an art and a science, and
+            understanding the scientific principles behind it can lead to a
+            consistently satisfying morning ritual. The journey begins with
+            selecting high-quality coffee beans, as the flavor profile is
+            greatly influenced by factors like origin and roast level.
+          </Text>
+          <Flex gap={fr(4)} mt="auto">
+            <Avatar size="sm" color="teal">
+              MP
+            </Avatar>
+            <Flex direction="column">
+              <Text
+                cl={(theme) =>
+                  theme.mode === "dark" ? ["base", 100] : ["base", 900]
+                }
+              >
+                Martin Petrov
+              </Text>
+              <Text
+                cl={(theme) =>
+                  theme.mode === "dark" ? ["base", 300] : ["base", 700]
+                }
+              >
+                Aug 29 2023
+              </Text>
+            </Flex>
+          </Flex>
+        </Stack>
+      </Card>
+      <Button onClick={() => toggleThemeMode()}>Toggle Theme Mode</Button>
+    </Center>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@fontsource/poppins/100.css";
+import "@fontsource/poppins/200.css";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/800.css";
+import "@fontsource/poppins/900.css";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { PrismaneProvider } from "@prismane/core";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <PrismaneProvider>
+      <App />
+    </PrismaneProvider>
   </React.StrictMode>
 );
 
